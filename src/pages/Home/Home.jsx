546cd4691fe41cb2a372cba3a848c143fab10870
Anastasia <anastasiakosovets@gmail.com>
2023-06-29 13:09:41 +0200

    feat: view of names of pokemon in Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,35 +5,35 @@ import { ProductCard } from "../../common/ProductCard/ProductCard";
 
 export const Home = () => {
 
-    const [pokemonList, setPokemonList] = useState([]);
+  const [pokemonList, setPokemonList] = useState([]);
 
-    useEffect(() => {
-      if(pokemonList.length === 0){
-        getAllPokemons()
-        .then( results => {
-          setPokemonList(results.data)
-        }).catch(error => console.log(error));
-      }
-    }, [pokemonList])
-    console.log("-----", pokemonList)
+  useEffect(() => {
+    if(pokemonList.length === 0){
+      getAllPokemons()
+      .then( results => {
+        setPokemonList(results.data.results)
+      }).catch(error => console.log(error));
+    }
+  }, [pokemonList])
+  console.log("-----", pokemonList)
 
-    return(
-        <div className="homeDesign">Pokemons:
-            { pokemonList.length > 0 ? (
-              <div>
-                {
-                  pokemonList.slice(0, 2).map(pokemonS => {
-                    return (
-                      <diV key={pokemonS.name}>
-                        {pokemonS.name}
-                      </diV>
-                    )
-                  })
-                }
-              </div>
-            ) : (
-              <div>Not Found...</div>
-            )}
-        </div>
-    )
+  return(
+      <div className="homeDesign">Pokemons:
+          { pokemonList.length > 0 ? (
+            <div>
+              {
+                pokemonList.slice(0, 20).map(pokemonS => {
+                  return (
+                    <diV key={pokemonS.name}>
+                      {pokemonS.name}
+                    </diV>
+                  )
+                })
+              }
+            </div>
+          ) : (
+            <div>Not Found...</div>
+          )}
+      </div>
+  )
 }
--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -3,5 +3,6 @@ import axios from "axios";
 
 export const getAllPokemons = async () => {
 
-    return await axios.get("https://pokeapi.co/api/v2/pokemon/");
+    // return await axios.get("https://pokeapi.co/api/v2/pokemon/?offset=40&limit=20");
+    return await axios.get("https://pokeapi.co/api/v2/pokemon/",);
 };
